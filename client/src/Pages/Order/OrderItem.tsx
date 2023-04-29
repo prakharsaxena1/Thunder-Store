8e94f5081ca385f2c9bdb757ca3882d9a5e0ade9
Prakhar Saxena <prakharsaxena2806@gmail.com>
2023-04-29 12:24:50 +0530

    completed orders UI

--- client/src/Pages/Order/OrderItem.tsx
+++ client/src/Pages/Order/OrderItem.tsx
@@ -7,34 +7,38 @@ const OrderItem: FC<any> = ({ order }) => {
   const deliveryStatus = order.dateDelivered ? `Delivered ${order.dateDelivered}` : 'pending';
   return (
     <Box sx={{ m: 1 }}>
-      <Stack direction="row" spacing={2}>
-        <Typography variant="body1">Delivery status:</Typography>
-        <Typography variant="body1">{deliveryStatus}</Typography>
-      </Stack>
       <Grid container component={Paper}>
         <Grid item xs={12} sm={3.5} md={2.5} lg={2} sx={{ p: 1, backgroundColor: '#ECF0F1' }}>
-          {/* Ship */}
-          <div>
-            <Typography variant="body2">Ship to:</Typography>
-            <Typography variant="body1">Prakhar Saxena</Typography>
-          </div>
-          {/* Total $ */}
-          <div>
-            <Typography variant="body2">Total amount:</Typography>
-            <Typography variant="body1">6124</Typography>
-          </div>
-          {/* Order placed on */}
-          <div>
-            <Typography variant="body2">Order placed on:</Typography>
-            <Typography variant="body1">21/02/2023</Typography>
-          </div>
+          <Stack direction="column" spacing={1}>
+            {/* Ship */}
+            <div>
+              <Typography variant="body2">Ship to:</Typography>
+              <Typography variant="body1">Prakhar Saxena</Typography>
+            </div>
+            {/* Total $ */}
+            <div>
+              <Typography variant="body2">Total amount:</Typography>
+              <Typography variant="body1">6124</Typography>
+            </div>
+            {/* Order placed on */}
+            <div>
+              <Typography variant="body2">Order placed on:</Typography>
+              <Typography variant="body1">21/02/2023</Typography>
+            </div>
+          </Stack>
         </Grid>
         <Grid item xs={12} sm={8.5} md={9.5} lg={10}>
           <Stack direction="row" justifyContent="start" sx={{ m: 1 }} spacing={2}>
-            <div style={{ maxWidth: '12vw', height: '80%' }}>
+            <div style={{ maxWidth: '7rem', maxHeight: '80%' }}>
               <img src={order.image} alt="order" style={{ maxWidth: '100%', maxHeight: '100%' }} />
             </div>
-            <Typography variant="h6">{order.title}</Typography>
+            <Stack direction="column">
+              <Typography variant="h6">{order.title}</Typography>
+              <Stack direction="row" justifyContent="start" spacing={1}>
+                <Typography variant="body2">Status:</Typography>
+                <Typography variant="body2">{deliveryStatus}</Typography>
+              </Stack>
+            </Stack>
           </Stack>
         </Grid>
       </Grid>
--- client/src/Pages/Order/index.tsx
+++ client/src/Pages/Order/index.tsx
@@ -21,6 +21,14 @@ const orders = [
     dateOrdered: new Date().toDateString(),
     // dateDelivered: new Date().toDateString(),
   },
+  {
+    id: '42',
+    image: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1600',
+    title: 'Pc parts',
+    price: 1940,
+    dateOrdered: new Date().toDateString(),
+    // dateDelivered: new Date().toDateString(),
+  },
 ];
 
 const Order: React.FC = () => {
@@ -28,7 +36,7 @@ const Order: React.FC = () => {
     <Box
       sx={{
         p: 2,
-        width: { sm: '95%', md: '85%', lg: '75%' },
+        width: { sm: '90%', md: '80%', lg: '70%' },
         m: '1rem auto',
       }}
     >
